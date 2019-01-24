a91b97575d4c1f549e1d5a8eedcabf65e110bdd5
Joe Ganobsik <jganobsik@gmail.com>
2019-01-24 16:25:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,12 +15,12 @@ get "/reversename/:name" do
     "#{@product}"
   end
   
-  get '/say/:number/:phrase'
-      @product = params[:number].to_i.times do 
-        params[:phrase].to_s
-      
-    "#{@product}"
-  end 
+#  get '/say/:number/:phrase'
+ #     @product = params[:number].to_i.times do 
+#        params[:phrase].to_s
+ #   
+#    "#{@product}"
+#  end 
   get '/say/:word1/:word2/:word3/:word4/:word5' 
   
    @product = params[:word1].to_s + ' ' +  params[:word2].to_s + ' ' + params[:word3].to_s + ' ' + params[:word4].to_s + ' ' + params[:word5].to_s
